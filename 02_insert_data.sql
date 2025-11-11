76e7457da78dfdac614b73bca56ef2bb1820d612
Cristian R. Godinez Limones <cristianlimones3@gmail.com>
2025-11-10 19:36:29 -0600

    Insert initial data into productos, departamento, and empleado

---
+++ 02_insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO productos(nombre,precio,cantidad) values 
+('teclado',180.00,5),
+('mouse',110.00,6),
+('mousepad',100.00,3),
+('monitor',2000.00,2);
+
+INSERT INTO departamento(nombre) values
+('IT'),('RRHH'),('CONTABILIDAD');
+
+INSERT INTO empleado(nombre,id_dep) values
+('Bryan Gamez',1),
+('Yareth Contreras',2),
+('Coral Dominguez',1),
+('Alex Vazquez',3);
